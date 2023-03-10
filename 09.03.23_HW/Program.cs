bbed98b0fd7aefebc51e34fd8da9ef5e9c2b8f07
Nikita Mohseni <mohseninikita9@gmail.com>
2023-03-10 17:27:21 +0200

    Добавил сериализацию

--- 09.03.23_HW/Program.cs
+++ 09.03.23_HW/Program.cs
@@ -5,25 +5,67 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+using System.Diagnostics;
+using System.Runtime.InteropServices.ComTypes;
+using serializer;
+using System.Runtime.Serialization.Json;
 
+namespace serializer
+{
+    [Serializable]
+    [DataContract]
+    public class Phone
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public int Price { get; set; }
+        
+        [DataMember]
+        public int Date { get; set; }
+
+        [DataMember]
+        public string Creator { get; set; }
+        public Phone(string name, int price, string creator, int date)
+        {
+            Name = name;
+            Price = price;
+            Creator = creator;
+            Date = date;
+        }
+        public Phone() { }
+
+        public override string ToString()
+        {
+            return $"Phone: {Creator}\nModel: {Name}\nPrice: {Price}\nDate: {Date}\n\n";
+        }
+    }
+}
 namespace _09._03._23_HW
 {
     internal class Program
     {
-        static void Main(string[] args)
+        #region XML_Func
+        public static void Add_XML(string temp)
         {
-            XDocument x = XDocument.Load("File.xml");
+            XDocument x = XDocument.Load(temp);
             XElement e = x.Root;
-            #region Add
             e.Add(new XElement("phone",
                 new XAttribute("name", "A72"),
                 new XElement("creator", "Samsung"),
                 new XElement("price", "12000"),
                 new XElement("date", "2020")));
-            x.Save("File.xml");
-            #endregion
+            x.Save(temp);
+        }
 
-            #region Search
+        public static void Search_XML(string temp)
+        {
+            XDocument x = XDocument.Load(temp);
+            XElement e = x.Root;
             var search = x.Element("phone")?
                 .Elements("phone")?
                 .FirstOrDefault(p => p.Attribute("name")?.Value == "A72");
@@ -32,9 +74,12 @@ namespace _09._03._23_HW
             var price_s = search?.Element("price")?.Value;
             var date_s = search?.Element("date")?.Value;
             Console.WriteLine($"Name: {name_s}  Age: {company_s}  Price: {price_s}  Date: {date_s}");
-            #endregion
+        }
 
-            #region Edit
+        public static void Edit_XML(string temp)
+        {
+            XDocument x = XDocument.Load(temp);
+            XElement e = x.Root;
             var tom = x.Element("phone")?
                 .Elements("phone")
                 .FirstOrDefault(p => p.Attribute("name")?.Value == "A72");
@@ -49,19 +94,28 @@ namespace _09._03._23_HW
                 if (company_e != null) company_e.Value = "40000";
                 var date_e = tom.Element("date");
                 if (company_e != null) company_e.Value = "2021";
-                x.Save("File.xml");
+                x.Save(temp);
             }
-            #endregion
+        }
 
-            #region Show
+        public static void Show_XML(string temp)
+        {
+            XDocument x = XDocument.Load(temp);
+            XElement e = x.Root;
+            var search = x.Element("phone")?
+                .Elements("phone")?
+                .FirstOrDefault(p => p.Attribute("name")?.Value == "A72");
             var name_sh = search?.Attribute("name")?.Value;
             var company_sh = search?.Element("creator")?.Value;
             var price_sh = search?.Element("price")?.Value;
             var date_sh = search?.Element("date")?.Value;
             Console.WriteLine($"Name: {name_sh}  Age: {company_sh}  Price: {price_sh}  Date: {date_sh}");
-            #endregion
+        }
 
-            #region Delete
+        public static void Delete_XML(string temp)
+        {
+            XDocument x = XDocument.Load(temp);
+            XElement e = x.Root;
             if (e != null)
             {
                 var bob = e.Elements("phone")
@@ -69,10 +123,43 @@ namespace _09._03._23_HW
                 if (bob != null)
                 {
                     bob.Remove();
-                    x.Save("File.xml");
+                    x.Save(temp);
                 }
             }
-            #endregion
+        }
+        #endregion
+
+        static void Main(string[] args)
+        {
+            Phone p = new Phone("A72", 42, "Samsung", 2021);
+            Phone p2 = new Phone("3123", 42, "12312", 2021);
+            List<Phone> list = new List<Phone>();
+            list.Add(p);
+            list.Add(p2);
+            FileStream stream = new FileStream("../../data.xml", FileMode.Create);
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Phone>));
+            serializer.Serialize(stream, list);
+            stream.Close();
+            stream = new FileStream("../../data.xml", FileMode.Open);
+            list = (List<Phone>)serializer.Deserialize(stream);
+            string s = String.Empty;
+            foreach (Phone j in list)
+                s += j.ToString();
+            Console.WriteLine(s);
+            stream.Close();
+
+            stream = new FileStream("../../list.json", FileMode.Create);
+            DataContractJsonSerializer jsonFormatter = new DataContractJsonSerializer(typeof(List<Phone>));
+            jsonFormatter.WriteObject(stream, list);
+            stream.Close();
+            stream = new FileStream("../../list.json", FileMode.Open);
+            list = (List<Phone>)jsonFormatter.ReadObject(stream);
+            s = String.Empty;
+            foreach (Phone j in list)
+                s += j.ToString();
+            Console.WriteLine(s);
+            stream.Close();
+
         }
     }
 }
